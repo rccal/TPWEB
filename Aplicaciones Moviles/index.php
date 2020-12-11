72d027402b261a6e08112d9ec22130df92497776
jeanch78 <cusipumajean20@gmail.com>
2020-12-11 10:00:56 -0500

    prueba2 html

--- Aplicaciones Moviles/index.php
+++ Aplicaciones Moviles/index.php
@@ -1,3 +1,14 @@
 <?php 
 echo "Hola JC";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h3>hola jc</h3>
+</body>
+</html>
