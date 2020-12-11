689775a3d64e253ce3f153468ae4ba5fe91aa6b4
Deus1520 <66262701+Deus1520@users.noreply.github.com>
2020-12-11 13:01:19 -0500

    .

--- ComercioElectronico/index.php
+++ ComercioElectronico/index.php
@@ -1,3 +1,12 @@
-<?php 
-"Hola desde aquí";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comercio Electronico</title>
+</head>
+<body>
+<h1>Comenrcio Electronico</h1>
+    
+</body>
+</html>
