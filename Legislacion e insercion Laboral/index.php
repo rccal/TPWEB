0a994358d1dbef48395a13a096c08ce532673151
DamianUscapi <dubcitouscapi@gmail.com>
2020-12-15 10:15:48 -0500

    actualizacion de damian

--- Legislacion e insercion Laboral/index.php
+++ Legislacion e insercion Laboral/index.php
@@ -8,8 +8,20 @@
 <body>
     <section>
         <div class="container">
-            <h1>¿que es la legislacion e insercion laboral</h1>
-            <p>conjunto de leyes y normas que tienen por objetivo regularizar las actividades laborales, ya sea en lo que respecta a los derechos del trabajador, como también a sus obligaciones y lo mismo para el empleador.</p>
+            <div class="curso">
+                <h1>¿que es la legislacion e insercion laboral</h1>
+                <p>
+                Entendemos por Normas Laborales o legislación laboral a aquel conjunto de leyes y normas que
+                tienen por objetivo regularizar las actividades laborales, ya sea en lo que respecta a los derechos del
+                trabajador, como también a sus obligaciones y lo mismo para el empleador.
+                </p>
+            </div>
+            <div class="objetivos">
+                <h2>objetivos y </h2>
+                <p>Reconocer, interpretar y aplicar el marco legal que describe los derechos y obligaciones en las
+                relaciones laborales para utilizar los procedimientos de inserción en el mercado laboral y las posibilidades
+                de empleo. </p>
+            </div>
         </div>
     </section>
 </body>
