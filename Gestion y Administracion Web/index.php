fc43c21915f3d60f60575fefacadf8de4d417c4f
Yuri930 <rojas97sanga@gmail.com>
2020-12-15 13:30:02 -0500

    Yuri

--- Gestion y Administracion Web/index.php
+++ Gestion y Administracion Web/index.php
@@ -1,3 +1,13 @@
-<?php 
-"Hola";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Gestion y Administracion Web</h1>
+    
+</body>
+
+</html>
