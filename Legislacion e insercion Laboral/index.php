071f0870e643766c2c599fcd7f670a739f03305e
DamianUscapi <dubcitouscapi@gmail.com>
2020-12-15 10:25:56 -0500

    actualizacion damian

--- Legislacion e insercion Laboral/index.php
+++ Legislacion e insercion Laboral/index.php
@@ -18,9 +18,40 @@
             </div>
             <div class="objetivos">
                 <h2>objetivos y </h2>
-                <p>Reconocer, interpretar y aplicar el marco legal que describe los derechos y obligaciones en las
-                relaciones laborales para utilizar los procedimientos de inserción en el mercado laboral y las posibilidades
-                de empleo. </p>
+                <ol>
+                    <li>
+                        Reconocer, interpretar y aplicar el marco legal que describe los derechos y obligaciones en las
+                        relaciones laborales para utilizar los procedimientos de inserción en el mercado laboral y las posibilidades
+                        de empleo.
+                    </li>
+                    <li>
+                        Interpreta, utiliza y valora las fuentes básicas de información del Derecho Laboral examinando los
+                        deberes y derechos que involucren a las partes
+                    </li>
+                    <li>
+                        Valora y establece rasgos como profesional y asume con responsabilidad y respeto la búsqueda de empleo
+                        e inserción en el mercado laboral.
+                    </li>
+                </ol>
+            </div>
+            <div class="temas">
+                <h2>Actividades de Aprendizaje</h2>
+                <ol>
+                    <li>Normas legales laborales</li>
+                    <li>Los derechos laborales:Jornada y horario de trabajo.</li>
+                    <li>Derechos de los trabajadores: Descansos remunerados y vacaciones.</li>
+                    <li>Normas legales laborales: Remuneración mínima vital y gratificaciones.</li>
+                    <li>Derechos de los trabajadores: Asignación familiar Bonificación por tiempo de servicios.</li>
+                    <li>La compensación por tiempo de servicio.</li>
+                    <li>Derecho de los trabajadores: Participación en las utilidades de la empresa</li>
+                    <li>Ley de Fomento del Empleo.</li>
+                    <li>Práctica Pre y Profesionales.</li>
+                    <li>a El mercado laboral</li>
+                    <li>FODA y marketing personal</li>
+                    <li>Taller Búsqueda de empleo – Ministerio de Trabajo</li>
+                    <li>Perfil profesional</li>
+                    <li>Proceso de selección de personal</li>
+                </ol>
             </div>
         </div>
     </section>
