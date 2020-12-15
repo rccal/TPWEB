a2f3f0dd972c54d2fd6539b51e29686996bf2bdc
djccistta <dcancinas.181ci07@istta.edu.pe>
2020-12-15 18:57:03 -0500

    TPW

--- Taller de Prgramacion Web/index.php
+++ Taller de Prgramacion Web/index.php
@@ -4,15 +4,118 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taller de Programación Web</title>
+      <!--Bootstrap-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+      <!--Fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;600&display=swap" rel="stylesheet">
+      <!--CSS-->
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
     
+    <section>
+        
+        <div class="row no-gutters">
+            <div class="col-md-1">
+                <div class="d-flex h-100">
+                    <div class="align-self-center mx-auto lead-xl">
+                        <ion-icon name="code-outline"></ion-icon>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-11">
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand text-light" href="#"> Taller de <span class="font-weight-bold"> Programación Web</span></a>
+                  </nav>
+            </div>
+        </div> 
+
+        <div class="row no-gutters">
+            <div class="col-md-1">
+                <div class="d-flex">
+                    <ul class="list-unstyled align-self-center mx-auto lead-xl">
+                        <li>
+                            <ion-icon name="code-download-outline"></ion-icon>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex ">
+                <div class="container align-self-center p-5">
+                    <h1 class="text-uppercase font-weight-bold mb-4">Servidor Web</h1>
+                    <p class="lead-xl mb-4">Un servidor es un dispositivo virtual que le brinda espacio y estructura a los sitios web para que almacenen sus datos y manejen sus páginas.</p>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <img src="img/servicio-web.png" alt="" height="500px">
+            </div>  
+        </div>
+
+        <div class="row no-gutters">
+            <div class="col-md-1">
+                <div class="d-flex">
+                    <ul class="list-unstyled align-self-center mx-auto lead-xl">
+                        <li>
+                            <ion-icon name="code-working-outline"></ion-icon>
+                        </li>  
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <img src="img/php.jpg" alt="" height="500px">
+            </div> 
+            <div class="col-md-4 d-flex ">
+                <div class="container align-self-center p-5">
+                    <h1 class="text-uppercase font-weight-bold mb-4">PHP</h1>
+                    <p class="lead-xl mb-4">es un lenguaje de código abierto muy popular especialmente adecuado para el desarrollo web y que puede ser incrustado en HTML.</p>
+                </div>
+            </div> 
+        </div>
+
+        <div class="row no-gutters">
+            <div class="col-md-1">
+                <div class="d-flex">
+                    <ul class="list-unstyled align-self-center mx-auto lead-xl">
+                        <li>
+                            <ion-icon name="bug-outline"></ion-icon>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex ">
+                <div class="container align-self-center p-5">
+                    <h1 class="text-uppercase font-weight-bold mb-4">MySQL</h1>
+                    <p class="lead-xl mb-4">Es un sistema de gestión de bases de datos relacionales de código abierto con un modelo cliente-servidor. </p>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <img src="img/mysql.jpg" alt="" height="500px">
+            </div>  
+        </div>
+
+        <div class="row no-gutters">
+            <div class="col-md-9">
+                
+            </div>
+
+            <div class="col-md-3 bg-light text-center">
+                <div class="p-4">
+                    <h3 class="font-weight-bold text-dark">Taller de Programación Web</h3>
+                </div>
+            </div>
+        </div>
+
+    </section>
 
 
 
-
+    <!--Bootstrap-->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <!--Iconos-->
+    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
 </body>
 </html>
