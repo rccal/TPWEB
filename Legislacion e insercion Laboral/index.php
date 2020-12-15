d84a185ade37b7c0b39de807edd63711af87165d
DamianUscapi <dubcitouscapi@gmail.com>
2020-12-15 09:52:43 -0500

    actualizar

--- Legislacion e insercion Laboral/index.php
+++ Legislacion e insercion Laboral/index.php
@@ -1,3 +1,11 @@
-<?php 
-"hola mundo";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Legislacion e Insercion Laboral</title>
+</head>
+<body>
+    
+</body>
+</html>
