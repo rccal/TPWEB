5a171a8b476e0f7b7a5586d085eb42b0d82c5fa9
jeanch78 <cusipumajean20@gmail.com>
2020-12-18 09:23:07 -0500

    gruardar todo

--- Aplicaciones Moviles/index.php
+++ Aplicaciones Moviles/index.php
@@ -4,11 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
     <title>Inicio</title>
 </head>
 <body>
     <h3>pagina web</h3>
     <h4>JEAN C prueba actualizar</h4>
     <h2>hola prueba</h2>
+
 </body>
 </html>
