c45428a4bb34831ff5c804542e33034128ad7940
DamianUscapi <dubcitouscapi@gmail.com>
2020-12-18 10:17:57 -0500

    actualizacion final

--- Legislacion e insercion Laboral/index.php
+++ Legislacion e insercion Laboral/index.php
@@ -18,12 +18,17 @@
     </style>
     <title>Legislacion e Insercion Laboral</title>
 </head>
-<body>
+<body class="bg-dark">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                 <li class="nav-item active">
                     <a class="nav-link" href="../index.php">inicio</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Legislacion e Insercion Laboral</a>
                 </li>
             </ul>
         </div>
@@ -74,12 +79,12 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <img src="../img/img1.jpg" class="w-80">
+                    <img src="../img/img1.jpg" class="im" width="100%" height="65%">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    <iframe width="480" height="270" src="https://www.powtoon.com/embed/ebAsCe3YpRY/" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="100%" height="100%" src="https://www.powtoon.com/embed/ebAsCe3YpRY/" frameborder="0" allowfullscreen></iframe>
                 </div>
                 <div class="col-md-6">
                     <div class="card">
@@ -104,6 +109,12 @@
                             </p>
                         </div>
                     </div>
+                </div>
+            </div>
+            <br>
+            <div class="row text-center">
+                <div class="offset-md-4 col-md-4">
+                    <h2 class="text-light border-bottom">actividades del curso</h2>
                 </div>
             </div>
             <br>
@@ -263,6 +274,7 @@
             </div>
         </div>
     </section>
+    <br>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
